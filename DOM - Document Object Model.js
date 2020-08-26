219361055e3d90306e74c81af40b8b014daf1d51
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-08-26 11:55:01 +0600

    DOM

---
+++ DOM - Document Object Model.js
@@ -0,0 +1,10 @@
+// <body>
+//   <h1 id="head1">This is Heading 1</h1>
+//  <h2 class="head2">This is Heading 2</h2>
+//  <p id="para1"><b>This is a paragraph</b></p>
+//   <script src="dom-script.js"></script>
+// </body>
+
+document.getElementById("head1").innerHTML = "LoL";
+document.getElementsByClassName("head2")
+document.getElementsByTagName("p")[0].innerHTML = "Lolabi";
